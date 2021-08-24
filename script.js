3288dd82481cfd55bd376ab97ba5e206e09a93a1
twodozeneggs <simonjohnloik@gmail.com>
2021-08-23 22:01:22 -0500

    Update script.js
    Wrote out first draft of pokemonlist

---
+++ script.js
@@ -0,0 +1,38 @@
+//pokedex... my favourite seven 
+PokemonList = [
+{
+    name: 'Quagsire',
+    height: 1.4,
+    types: ['water', 'ground'] 
+},
+{
+    name: 'Arcanine',
+    height: 1.9,
+    types: ['fire'],
+}
+{
+    name: "Breloom",
+    height: 1.2,
+    types: ['grass', 'fighting']
+}
+{
+    name: 'Haunter',
+    height: 1.6,
+    types: ['posion', 'ghost']
+}
+{
+    name: 'Dragonite',
+    height: 2.2,
+    types: ['dragon', 'flying']
+}
+{
+    name: 'Armaldo',
+    height: 1.5,
+    types: ['bug', 'rock']
+}
+{
+    name: 'Krokorok',
+    height: 1,
+    types: ['ground', 'dark']
+}
+]
