1001f39755b9a6f312021a025314feee06e00e53
twodozeneggs <simonjohnloik@gmail.com>
2021-08-26 17:22:52 -0500

    Title Change
    Title change

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //pokedex... my favourite seven 
-PokemonList = [
+let pokemonRepository = (function() { 
+let PokemonList = [
 {
     name: 'Quagsire',
     height: 1.4,
@@ -9,30 +10,46 @@ PokemonList = [
     name: 'Arcanine',
     height: 1.9,
     types: ['fire'],
-}
+},
 {
-    name: "Breloom",
+    name: 'Breloom',
     height: 1.2,
     types: ['grass', 'fighting']
-}
+},
 {
     name: 'Haunter',
     height: 1.6,
     types: ['posion', 'ghost']
-}
+},
 {
     name: 'Dragonite',
     height: 2.2,
     types: ['dragon', 'flying']
-}
+},
 {
     name: 'Armaldo',
     height: 1.5,
     types: ['bug', 'rock']
-}
+},
 {
     name: 'Krokorok',
     height: 1,
     types: ['ground', 'dark']
 }
-]
+];
+
+for (let i = 0; i < PokemonList.length; i++) {
+    let reaction = "";
+    let PokemonHeight = pokemonList[i].height;
+
+    if (PokemonHeight < 1) {
+        reaction =  "Wow what a cutie!";
+    } else if (PokemonHeight >1 && PokemonHeight <= 5){
+        reaction = "This one is pretty big";
+    }
+    document.write(
+        pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - '+ reaction + '<br>'
+    );
+
+    }
+
